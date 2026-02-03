ab4b78766af57de7914efa4590f3cb15162a62b6
arnabp-x64 <arnabp586@gmail.com>
2026-02-03 21:30:21 +0530

    Added more features

--- BMIChecker/BMIChecker.cpp
+++ BMIChecker/BMIChecker.cpp
@@ -12,39 +12,104 @@ using namespace std::this_thread;
 
 
 void TypeWriter(const string text, int delay=30) {
-    for (char c : text) {
-        cout << c << flush;
-        sleep_for(milliseconds(delay));
-    }
+	for (char c : text) {
+		cout << c << flush;
+		sleep_for(milliseconds(delay));
+	}
 }
 
 
 int main(){
 
-    TypeWriter("=============================");
-    cout << endl << setw(4);
-    TypeWriter("Welcome to BMIChecker");
-    cout << endl;
-    TypeWriter("=============================");
-    cout << endl << endl;
-
-    TypeWriter("Instructions:\n");
-    TypeWriter("1. Type your Age \n");
-    TypeWriter("2. Type your Height in miters\n");
-    TypeWriter("3. Type your Weight in Kilograms\n");
 
 
+	TypeWriter("=============================");
+	cout << endl << setw(4);
+	TypeWriter("Welcome to BMIChecker");
+	cout << endl;
+	TypeWriter("=============================");
+	cout << endl << endl;
+	
+	sleep_for(seconds(2));
 
-    return 0;
+	TypeWriter("Instructions:\n");
+	cout << endl;
+	sleep_for(seconds(1));
+	TypeWriter("2. Type your Height in miters(If in other scale write hight and unit,like 5.1 ft)\n");
+	sleep_for(seconds(1));
+	TypeWriter("3. Type your Weight in Kilograms\n");
+	sleep_for(seconds(1));
+	TypeWriter("       - Press enter and see the magic...\n");
+	cout << endl;
+	cin.get();
+	bool YN = true;
+
+	long double height, weight, bmi;
+	string Unit;
+	
+
+	while (YN) {
+
+		
+		TypeWriter("Enter your Height+Unit: ");
+		cin >> height >> Unit;
+
+		TypeWriter("Enter your Weight (in kilograms): ");
+		cin >> weight;
+
+		if (Unit == "cm" || Unit == "centimeters") {
+			height = height / 100;
+		}
+		else if (Unit == "mm" || Unit == "millimeters") {
+			height = height / 1000;
+		}
+		else if (Unit == "in" || Unit == "inches") {
+			height = height * 0.0254;
+		}
+		else if (Unit == "ft" || Unit == "feet") {
+			height = height * 0.3048;
+		}
+		else if (Unit == "yd" || Unit == "yards") {
+			height = height * 0.9144;
+		}
+
+
+
+		bmi = weight / (height * height);
+
+		string bmix = to_string(bmi);
+		if (bmi < 18.5) {
+			TypeWriter("\nYour BMI is " + bmix + ", you are underweight.\n");
+		}
+		else if (bmi >= 18.5 && bmi < 24.9) {
+			TypeWriter("\nYour BMI is " + bmix + ", you have a normal weight.\n");
+		}
+		else if (bmi >= 25 && bmi < 29.9) {
+			TypeWriter("\nYour BMI is " + bmix + ", you are overweight.\n");
+		}
+		else {
+			TypeWriter("\nYour BMI is " + bmix + ", you are obese.\n");
+		}
+
+		cout << endl;
+
+		TypeWriter("Do you want to calculate again? (y/n): ");
+		char response;
+		cin >> response;
+		if (response == 'y' || response == 'Y') {
+			YN = true;
+			cout << endl;
+		}
+		else {
+			YN = false;
+			cout << endl;
+			TypeWriter("Thank you for using BMIChecker. Goodbye!\n");
+			cout << endl;
+			cin.ignore();
+			cin.get();
+		}
+	}
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # BMIChecker
+
+## hi
+
